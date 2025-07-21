14c2d72352df452ff9222734a0a7e0d1645285c8
AshokPabra <ashokpabra1858@gmail.com>
2025-07-21 15:49:09 +0530

    Make the TotalDistanceInMeters function Unexported

--- DistanceUnit/distanceUnit.go
+++ DistanceUnit/distanceUnit.go
@@ -32,7 +32,7 @@ func (d1 *distanceUnit) Add(d2 *distanceUnit) *distanceUnit {
 	if d1.unit == kilometers {
 		return d1.TotalDistanceInKilometers(d2)
 	}
-	return d1.TotalDistanceInMeters(d2)
+	return d1.totalDistanceInMeters(d2)
 }
 
 func NewDistanceUnit(distance float64, unit Unit) (*distanceUnit, error) {
@@ -55,7 +55,7 @@ func (d *distanceUnit) Inmeter() *distanceUnit {
 	return d
 }
 
-func (d1 *distanceUnit) TotalDistanceInMeters(d2 *distanceUnit) *distanceUnit {
+func (d1 *distanceUnit) totalDistanceInMeters(d2 *distanceUnit) *distanceUnit {
 	return &distanceUnit{distance: d1.Inmeter().distance + d2.Inmeter().distance, unit: meters}
 }
 
