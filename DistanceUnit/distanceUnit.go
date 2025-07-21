e7314fbfc0264ee7d7796924a51aaacc7c9c5577
AshokPabra <ashokpabra1858@gmail.com>
2025-07-21 14:04:53 +0530

    Refactored EqualityOfDistance function
    Co-Authored by Ashok
    Co-Authored by Swarathmica

--- DistanceUnit/distanceUnit.go
+++ DistanceUnit/distanceUnit.go
@@ -25,49 +25,59 @@ func NewDistanceUnit(distance int, unit Unit) (*distanceUnit, error) {
 }
 
 func (d1 *distanceUnit) EqualityOfDistance(d2 *distanceUnit) bool {
-	if d1.distance == d2.distance && d1.unit == d2.unit {
-		return true
-	} else if d1.unit == kilometers {
-		d1 = d1.KilometerToMeter()
-		if d2.unit == meters {
-			if d1.distance == d2.distance && d1.unit == d2.unit {
-				return true
-			}
-		} else if d2.unit == centimeters {
-			d2 = d2.CentimeterToMeter()
-			if d1.distance == d2.distance && d1.unit == d2.unit {
-				return true
-			}
-		}
+	// if d1.distance == d2.distance && d1.unit == d2.unit {
+	// 	return true
+	// } else if d1.unit == kilometers {
+	// 	d1 = d1.KilometerToMeter()
+	// 	if d2.unit == meters {
+	// 		if d1.distance == d2.distance && d1.unit == d2.unit {
+	// 			return true
+	// 		}
+	// 	} else if d2.unit == centimeters {
+	// 		d2 = d2.CentimeterToMeter()
+	// 		if d1.distance == d2.distance && d1.unit == d2.unit {
+	// 			return true
+	// 		}
+	// 	}
 
-	} else if d2.unit == kilometers {
-		d2 = d2.KilometerToMeter()
-		if d1.unit == meters {
-			if d1.distance == d2.distance && d1.unit == d2.unit {
-				return true
-			}
-		} else if d1.unit == centimeters {
-			d1 = d1.CentimeterToMeter()
-			if d1.distance == d2.distance && d1.unit == d2.unit {
-				return true
-			}
-		}
-	} else if d1.unit == centimeters {
-		d1 = d1.CentimeterToMeter()
-		if d2.unit == meters {
-			if d1.distance == d2.distance && d1.unit == d2.unit {
-				return true
-			}
-		}
-	} else if d1.unit == meters {
-		if d2.unit == centimeters {
-			d2 = d2.CentimeterToMeter()
-			if d1.distance == d2.distance && d1.unit == d2.unit {
-				return true
-			}
-		}
+	// } else if d2.unit == kilometers {
+	// 	d2 = d2.KilometerToMeter()
+	// 	if d1.unit == meters {
+	// 		if d1.distance == d2.distance && d1.unit == d2.unit {
+	// 			return true
+	// 		}
+	// 	} else if d1.unit == centimeters {
+	// 		d1 = d1.CentimeterToMeter()
+	// 		if d1.distance == d2.distance && d1.unit == d2.unit {
+	// 			return true
+	// 		}
+	// 	}
+	// } else if d1.unit == centimeters {
+	// 	d1 = d1.CentimeterToMeter()
+	// 	if d2.unit == meters {
+	// 		if d1.distance == d2.distance && d1.unit == d2.unit {
+	// 			return true
+	// 		}
+	// 	}
+	// } else if d1.unit == meters {
+	// 	if d2.unit == centimeters {
+	// 		d2 = d2.CentimeterToMeter()
+	// 		if d1.distance == d2.distance && d1.unit == d2.unit {
+	// 			return true
+	// 		}
+	// 	}
+	// }
+	// return false
+	return d1.Inmeter().distance == d2.Inmeter().distance
+}
+
+func (d *distanceUnit) Inmeter() *distanceUnit {
+	if d.unit == centimeters {
+		d = &distanceUnit{distance: d.distance / 100, unit: meters}
+	} else if d.unit == kilometers {
+		d = &distanceUnit{distance: d.distance * 1000, unit: meters}
 	}
-	return false
+	return d
 }
 
 func (d *distanceUnit) KilometerToMeter() *distanceUnit {
