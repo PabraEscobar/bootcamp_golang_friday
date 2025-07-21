1966a72f58b70f88a5d7ca3f665898c74b7df784
AshokPabra <ashokpabra1858@gmail.com>
2025-07-21 19:15:47 +0530

    Refactor: Add comments for the functions
    Co-Authored by Ashok
    Co-Authored by Swarathmica

--- Distance/distance.go
+++ Distance/distance.go
@@ -18,32 +18,32 @@ type distance struct {
 	unit  Unit
 }
 
-func (d1 distance) toString() string {
+func (d1 distance) toString() string { //for displaying structure in a readable format
 	return fmt.Sprintf("value %f %s", d1.value, d1.unit)
 }
 
 func (d1 *distance) Add(d2 *distance) *distance {
 	m := distanceMap()
-	return &distance{value: (d1.Inmeter().value + d2.Inmeter().value) / m[d1.unit], unit: d1.unit}
+	return &distance{value: (d1.Inmeter().value + d2.Inmeter().value) / m[d1.unit], unit: d1.unit} //converting both distance values to  meter and back to the left operand's unit
 }
 
-func NewDistanceUnit(value float64, unit Unit) (*distance, error) {
+func NewDistanceUnit(value float64, unit Unit) (*distance, error) { //creating new Distance struct
 	if value < 0 {
 		return nil, errors.New("distance cannot be negative")
 	}
 	return &distance{value: value, unit: unit}, nil
 }
 
-func (d1 *distance) equals(d2 *distance) bool {
+func (d1 *distance) equals(d2 *distance) bool { //Checking equality between the distances
 	return d1.Inmeter().value == d2.Inmeter().value
 }
 
-func (d *distance) Inmeter() *distance {
+func (d *distance) Inmeter() *distance { //Converting all distances to meter
 	m := distanceMap()
 	return &distance{value: d.value * m[d.unit], unit: meters}
 }
 
-func distanceMap() map[Unit]float64 {
+func distanceMap() map[Unit]float64 { //Distance map for conversion of units to meter
 	m := make(map[Unit]float64)
 	m[kilometers] = 1000
 	m[meters] = 1
