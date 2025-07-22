4f3bdf78b3eb222628ab91606ee65121b23dd91c
AshokPabra <ashokpabra1858@gmail.com>
2025-07-22 16:23:03 +0530

    Refactor: rename to baseUnit

--- Distance/distance.go
+++ Distance/distance.go
@@ -24,7 +24,7 @@ func (d1 distance) toString() string { //for displaying structure in a readable 
 
 func (d1 *distance) Add(d2 *distance) *distance {
 	m := distanceMap()
-	return &distance{value: (d1.Inmeter().value + d2.Inmeter().value) / m[d1.unit], unit: d1.unit} //converting both distance values to  meter and back to the left operand's unit
+	return &distance{value: (d1.InBase().value + d2.InBase().value) / m[d1.unit], unit: d1.unit} //converting both distance values to  meter and back to the left operand's unit
 }
 
 func NewDistanceUnit(value float64, unit Unit) (*distance, error) { //creating new Distance struct
@@ -35,10 +35,10 @@ func NewDistanceUnit(value float64, unit Unit) (*distance, error) { //creating n
 }
 
 func (d1 *distance) equals(d2 *distance) bool { //Checking equality between the distances
-	return d1.Inmeter().value == d2.Inmeter().value
+	return d1.InBase().value == d2.InBase().value
 }
 
-func (d *distance) Inmeter() *distance { //Converting all distances to meter
+func (d *distance) InBase() *distance { //Converting all distances to meter
 	m := distanceMap()
 	return &distance{value: d.value * m[d.unit], unit: meters}
 }
